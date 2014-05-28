5d353e91a468c30946f992d47de5ed367385f308
Daniel Heinrich <danusestheinternet@gmail.com>
2014-05-27 22:07:36 -0700

    Initial Upload

---
+++ run.py
@@ -0,0 +1,28 @@
+#
+# <( 'v' )> Work in Progress <( 'v' )>
+#
+
+import config
+
+import webapp2
+import jinja2
+
+
+class Home(webapp2.RequestHandler):
+	
+	def get(self):
+
+		template = config.JINJA_ENVIRONMENT.get_template('templates/home.html')
+		return self.response.write(template.render())
+
+
+
+#
+# Run the application
+#
+
+application = webapp2.WSGIApplication([
+
+	('/', Home)
+
+])
